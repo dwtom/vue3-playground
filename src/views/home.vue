1eb4618532519849fcd1f33eaebb9ad21bf54de2
dwtom <1119086531@qq.com>
2023-03-24 10:49:32 +0800

    feat: 修改基础配置并删除不用的文件

---
+++ src/views/home.vue
@@ -0,0 +1,16 @@
+<!--
+ * @Description: 首页
+ * @Author: Dong Wei
+ * @Date: 2023-03-24 10:21:16
+ * @LastEditors: Dong Wei
+ * @LastEditTime: 2023-03-24 10:35:33
+ * @FilePath: \vue3-playground\src\views\home.vue
+-->
+<template>
+  <div>
+    <el-button plain>首页按钮</el-button>
+  </div>
+</template>
+
+<script setup lang="ts"></script>
+<style scoped></style>
--- vite.config.ts
+++ vite.config.ts
@@ -1,19 +1,50 @@
 import { fileURLToPath, URL } from 'node:url';
 
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 import vue from '@vitejs/plugin-vue';
 import vueJsx from '@vitejs/plugin-vue-jsx';
 
+import Components from 'unplugin-vue-components/vite';
+import AutoImport from 'unplugin-auto-import/vite';
+import { ElementPlusResolver } from 'unplugin-vue-components/resolvers';
+
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [vue(), vueJsx()],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url)),
+export default defineConfig(({ mode }) => {
+  // 根据当前工作目录中的 `mode` 加载 .env 文件
+  const viteEnv = loadEnv(mode, process.cwd());
+  const isProduction = viteEnv.VITE_USER_NODE_ENV === 'production';
+  return {
+    css: {
+      devSourcemap: true,
     },
-  },
-  server: {
-    port: 10010,
-    open: true,
-  },
+    plugins: [
+      vue(),
+      vueJsx(),
+      Components({ resolvers: [ElementPlusResolver()], dts: true }),
+      AutoImport({
+        imports: ['vue', 'vue-router'],
+        resolvers: [ElementPlusResolver()],
+        dts: true,
+        eslintrc: {
+          enabled: true,
+        },
+      }),
+    ],
+    resolve: {
+      alias: {
+        '@': fileURLToPath(new URL('./src', import.meta.url)),
+      },
+    },
+    esbuild: {
+      // 打包后去除打印及断点
+      pure: isProduction ? ['console.log', 'debugger'] : [],
+    },
+    build: {
+      reportCompressedSize: false, // 禁用 gzip 压缩大小报告 提高构建速度
+    },
+    server: {
+      port: 10010,
+      open: true,
+    },
+  };
 });
